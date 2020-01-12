360c1d93ec1f3c8660c8a829b259bcfc3d814a8f
sashachepuha <shizadance69@gmail.com>
2020-01-12 15:13:15 +0300

    Create inheritance.py
    Пример наследования

---
+++ inheritance.py
@@ -0,0 +1,25 @@
+class PC (object):
+    def __init__(self):
+        self.model =''
+        self.speed = ''
+
+    def ShowSpeed (self):
+        print(self.speed + self.model)
+
+class Laptop (PC):
+    # Наследуем клас Laptop от класса PC
+    def __init__(self):
+        super(Laptop, self).__init__()
+        # функция super() пробежит по всемродителям класса Laptop
+        # и добавит все свойства __init__ не дублируя
+
+    def ShowSpeed (self):
+        # создаем такую же функцию как у родителя
+        if True:
+            pass
+            # новые действия для функции
+        else:
+            super(Laptop, self).ShowSpeed()
+            # выполняем функцию родителя
+    def ShowModel(self):
+        print('Laptop')
